a09d7a49eb9e77744f717ce0cce14e75a57836b5
BrunoVollin <bruno.3av@gmail.com>
2021-06-01 20:50:51 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "./style.css"
+import Emf from "./Pages/Emf"
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Emf />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
